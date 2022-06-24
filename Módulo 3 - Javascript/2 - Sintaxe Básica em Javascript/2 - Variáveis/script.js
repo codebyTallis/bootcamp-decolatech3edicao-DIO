5580d495dc256d16fe60d18961ebdf7c2001f7fb
codebyTallis <tallisbreanad@gmail.com>
2022-06-24 00:25:43 -0300

    Sintaxe básica em JS

---
+++ Módulo 3 - Javascript/2 - Sintaxe Básica em Javascript/2 - Variáveis/script.js
@@ -0,0 +1,99 @@
+//TIPOS PRIMITIVOS
+
+//boleanos
+var vOuF = false;
+console.log(typeof(vOuF));
+
+//number
+var numeroQualquer = 1;
+console.log(typeof(numeroQualquer));
+
+//string
+var nome = 'Tallis';
+console.log(typeof(nome));
+
+//function
+var funcao = function() {}
+console.log(typeof(funcao));
+
+//como declarar
+var variavel = 'Tallis';
+variavel = 'Brean';
+console.log(variavel);
+
+let variavel2 = 'Tallis';
+variavel2 = 'Brean';
+console.log(variavel2);
+
+/*const constante = 'Tallis';
+constante = 'Tallis';
+console.log(constante);
+
+var escopoGlobal = 'Global';
+console.log(escopoGlobal);
+
+function escopoGlobal() {
+    let escopoLocalInterno = 'local';
+    console.log(escopoLocalInterno);
+}
+escopoGlobal();*/
+
+// atribuição
+var atribuição = 'Tallis';
+
+//comparação
+var comparacao = '0' == 0;
+console.log(comparacao);
+
+//comparacao identica
+var comparacaoIdentica = '0' === 0;
+console.log(comparacaoIdentica);
+
+//adição
+var adicao = 20 + 45;
+console.log(adicao);
+
+//subtração
+var subtracao = 89 - 37;
+console.log(subtracao);
+
+// multiplicação
+var multiplicacao = 2 * 15;
+console.log(multiplicacao);
+
+// divisão real
+var divisaoReal = 150 / 3;
+console.log(divisaoReal);
+
+// divisão inteira
+var divisaoInteira = 5 % 2;
+console.log(divisaoInteira);
+
+// potenciação
+var potenciacao = 2 ** 10;
+console.log(potenciacao);
+
+//maior que
+var maiorQue = 8 > 2;
+console.log(maiorQue);
+
+//menor que
+var menorQue = 10 < 80;
+console.log(menorQue);
+
+// maior ou igual a 
+var maiorOuIgual = 5 >= 2;
+console.log(maiorOuIgual);
+
+// menor ou igual a 
+var menorOuIgual = 5 <= 2;
+console.log(menorOuIgual);
+
+var e = true && true;
+console.log(e);
+
+var ou = false || false;
+console.log(ou);
+
+var nao = !true;
+console.log(nao);
---
+++ Módulo 3 - Javascript/2 - Sintaxe Básica em Javascript/5 - Funções/script.js
@@ -0,0 +1,17 @@
+// funções - functions
+
+// como declarar
+// deve ser declarada usando a palavra function seguida de "()" e "{}"
+ function funcao() {
+    console.log('Mensagem de uma função');
+}
+// chamando a função
+funcao();
+
+// função com parâmetros
+// definindo parâmetros
+function mensagem(primeiro, segundo) {
+    console.log(primeiro, segundo)
+}
+// chamando a função atribuindo valor aos parâmetros
+mensagem('Tudo certo', 'jovem!');
