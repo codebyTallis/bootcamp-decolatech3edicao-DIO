06da3e0283ca58635ef51bcc0b415cac36bc99bf
codebyTallis <tallisbreanad@gmail.com>
2022-06-27 20:34:36 -0300

    Atividades variáveis e tipos

---
+++ Módulo 3 - Javascript/4 - Variáveis e Tipos/AtividadeSubstituirPares.js
@@ -0,0 +1,18 @@
+function subPares(array) {
+    if (!array) return -1;
+    if (!array.length) return -1;
+
+    for (let i = 0; i < array.length; i ++) {
+        if (array[i] === 0){
+            console.log("Você já é zero!");
+        } else if (array[i] % 2 === 0) {
+            console.log(`Substituindo ${array[i]} por 0...`);
+            array[i] = 0;
+        }
+    }
+
+    return array;
+}
+
+let arr = [1, 4, 6, 80, 44, 23, 34, 30, 90, 55, 36];
+console.log(subPares(false));
